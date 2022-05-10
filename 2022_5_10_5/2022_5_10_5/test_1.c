d1480663a4c418bdfab3e73ae5b3bf50238e9cee
wei-mohe <1330359162@qq.com>
2022-05-10 18:22:59 +0800

    练习

---
+++ 2022_5_10_5/2022_5_10_5/test_1.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main() {
+    int n = 0;
+    
+    int i = 0;
+    int j = 0;
+    while (scanf("%d", &n) != EOF) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if ((i == j) || (i + j == n - 1)) {
+                    printf("*");
+                }
+                else {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+        /*for (i = n / 2; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if ((i == j) || (i + j == n - 1)) {
+                    printf("*");
+                }
+                else {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }*/
+    }
+    
+    return 0;
+}
+//*    *
+// *  * 
+//  **  
+//  **  
+// *  * 
+//*    *
