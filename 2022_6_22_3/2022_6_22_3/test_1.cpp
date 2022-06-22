e00c224401b6d88fd7689e42ad1e1af75b0c7214
wei-mohe <1330359162@qq.com>
2022-06-22 23:29:23 +0800

    c++

---
+++ 2022_6_22_3/2022_6_22_3/test_1.cpp
@@ -0,0 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "he he!";
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << 2 + 4 << " " << 10 - 2 - 4;
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << 14 / 4 << endl;
+//	cout << 14 / 4 * 4 << endl;
+//	cout << 14 - 14 / 4 * 4 << endl;
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << 500.0 / 3 << endl;
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << 500.0 / 3 << endl;
+//	cout << 5000000.0 / 3 << endl;
+//	cout << 0.000005 / 3 << endl;
+//	cout << 5e6 / 3 + 5e-6 / 3 - 5e6 / 3 << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << (260 + 220) / (12 + 20) << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//
+//int main() {
+//	cout << sqrt(pow(6, 2) + pow(9, 2)) << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int balance = 100;
+//	balance += 10;
+//	cout << balance << endl;
+//	balance -= 20;
+//	cout << balance << endl;
+//	balance = 0;
+//	cout << balance << endl;
+//
+//
+//	return 0;
+//}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+#define PI 3.141593
+//const double PI = 3.141593;
+int main() {
+	double r = 5;
+	cout << 2 * PI * r << endl;
+	cout << PI * r * r << endl;
+	cout << (4.0 * PI * pow(r, 3)) / 3 << endl;
+	return 0;
+}
